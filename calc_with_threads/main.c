887404a50a25c65f2bd229dc85727e19cfa31d36
Vladislav <you@example.com>
2017-09-30 17:40:41 +0300

    To be continued

--- calc_with_threads/main.c
+++ calc_with_threads/main.c
@@ -7,9 +7,52 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+typedef struct {
+    int begin;
+    int end;
+    int result;
+}threadTask;
+
+static int calc() {
+    
+    return calc();
+}
+
+int main(int argc, const char * argv[])
+{
+    int result;
+    int left = atoi(argv[1]);
+    int right = atoi(argv[2]);
+    int NUMBER_OF_THREADS = atoi(argv[3]);
+    unsigned long sigma = 0;
+
+    threadTask* tasks = malloc(NUMBER_OF_THREADS * sizeof(threadTask));
+    pthread_t* threadId = malloc(NUMBER_OF_THREADS* sizeof(pthread_t));
+    
+    int loadPerThread = (right - left) / NUMBER_OF_THREADS;
+    
+    for (int i = 0; i < NUMBER_OF_THREADS; i++)
+    {
+        tasks[i].begin = left + i * loadPerThread;
+        tasks[i].end = tasks[i].begin + loadPerThread;
+        result = pthread_create(threadId+i, NULL, calc(), (void*) &tasks[i]);
+        if (result)
+        {
+            printf("Can't create thread %d, returned value = %d\n", i,result);
+            exit(-1);
+        }
+    }
+    
+    int delta = right - left + loadPerThread * NUMBER_OF_THREADS;
+    for (int i = delta; i <= right; i++)
+    {
+        sigma = sigma + delta;
+    }
+    
     return 0;
 }
