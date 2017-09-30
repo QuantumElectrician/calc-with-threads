eeda159ccba886108ccf56bf19fdf16f55cd8268
Vladislav <you@example.com>
2017-09-30 16:56:07 +0300

    Initial Commit

---
+++ calc_with_threads/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calc_with_threads
+//
+//  Created by Владислав Агафонов on 30.09.2017.
+//  Copyright © 2017 Владислав Агафонов. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
